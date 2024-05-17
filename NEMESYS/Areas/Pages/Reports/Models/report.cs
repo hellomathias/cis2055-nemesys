c3d40718b87d8473f9184a54da8b2c0a593ff5dd
N8N2212 <120168867+N8N2212@users.noreply.github.com>
2024-05-17 18:17:32 +0200

    Add files via upload

---
+++ NEMESYS/Areas/Pages/Reports/Models/report.cs
@@ -0,0 +1,19 @@
+﻿namespace NEMESYS.Areas.Pages.Reports.Models
+{
+
+    public class Report
+    {
+        public DateTime DateOfReport { get; set; }
+        public string Location { get; set; }
+        public DateTime DateSpotted { get; set; }
+        public string TimeSpotted { get; set; } // Assuming TimeSpotted is a string representing time.
+        public string TypeOfHazard { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; } = "Open"; // Default to "Open"
+        public string ReporterEmail { get; set; }
+        public string ReporterPhone { get; set; } // Optional
+        public string OptionalPhotoPath { get; set; } // Path to the optional photo
+        public int Upvotes { get; set; }
+    }
+
+}
