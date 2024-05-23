a66b01eaac276c46ffe67669b144d8bcbf778339
Mathias Schembri <mathiasschembri@gmail.com>
2024-05-23 22:16:52 +0200

    Add ConfirmationModel for report submission page

---
+++ NEMESYS/Areas/Identity/Pages/Reports/Confirmation.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NEMESYS.Areas.Identity.Pages.Reports
+{
+    public class ConfirmationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
