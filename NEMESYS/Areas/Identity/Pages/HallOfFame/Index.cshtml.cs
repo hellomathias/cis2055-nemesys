1d04b19b33a5daaed1b3e63e7e493331fef77ef2
N8N2212 <120168867+N8N2212@users.noreply.github.com>
2024-05-28 16:56:05 +0200

    Add files via upload
    start to the hall of fame page, which displays the data of the highest ranked reporters in a table format, once connected successfully with the database

---
+++ NEMESYS/Areas/Identity/Pages/HallOfFame/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NEMESYS.Areas.Identity.Pages.HallOfFame
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ NEMESYS/Areas/Identity/Pages/HallOfFame/Models/ReportRanking.cs
@@ -0,0 +1,9 @@
+﻿namespace NEMESYS.Areas.Identity.Pages.HallOfFame.Models
+{
+	public class ReporterRanking
+	{
+		public string ReporterName { get; set; }
+		public int ReportCount { get; set; }
+	}
+
+}
