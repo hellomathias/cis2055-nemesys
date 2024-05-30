ff9b6ed1b771d371ae59330716e5d41faa900351
Mathias Schembri <mathiasschembri@gmail.com>
2024-05-30 09:55:36 +0200

    Reformat ReporterRanking Class for Consistency

--- NEMESYS/Areas/Identity/Pages/HallOfFame/Models/ReportRanking.cs
+++ NEMESYS/Areas/Identity/Pages/HallOfFame/Models/ReportRanking.cs
@@ -1,9 +1,8 @@
 ﻿namespace NEMESYS.Areas.Identity.Pages.HallOfFame.Models
 {
-	public class ReporterRanking
-	{
-		public string ReporterName { get; set; }
-		public int ReportCount { get; set; }
-	}
-
+    public class ReporterRanking
+    {
+        public string ReporterName { get; set; }
+        public int ReportCount { get; set; }
+    }
 }
