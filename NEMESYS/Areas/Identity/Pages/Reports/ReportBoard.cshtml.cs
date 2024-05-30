da287b8467124d2d9f0e8fa98d42512b16a158d4
Mathias Schembri <mathiasschembri@gmail.com>
2024-05-30 10:07:07 +0200

    Implement Upvote Feature and Investigator Data on Report Board

--- NEMESYS/Areas/Identity/Pages/Reports/ReportBoard.cshtml.cs
+++ NEMESYS/Areas/Identity/Pages/Reports/ReportBoard.cshtml.cs
@@ -1,13 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using NEMESYS.Areas.Identity.Data;
+using NEMESYS.Areas.Identity.Pages.Investigations.Models;
 using NEMESYS.Areas.Identity.Pages.Reports.Models;
+using NEMESYS.Data;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
-using NEMESYS.Data;
 
 namespace NEMESYS.Areas.Identity.Pages.Reports
 {
@@ -23,29 +24,60 @@ namespace NEMESYS.Areas.Identity.Pages.Reports
         }
 
         public IList<Report> Reports { get; set; }
+        public IList<Investigation> Investigations { get; set; }
+        public IList<Upvote> Upvotes { get; set; }
+        public Dictionary<int, bool> UserUpvotes { get; set; } = new Dictionary<int, bool>();
 
         public async Task OnGetAsync()
         {
             Reports = await _context.Reports.ToListAsync();
+            Investigations = await _context.Investigations.ToListAsync();
+            Upvotes = await _context.Upvotes.ToListAsync();
+
+            var userId = _userManager.GetUserId(User);
+            if (userId != null)
+            {
+                UserUpvotes = await _context.Upvotes
+                    .Where(u => u.UserId == userId)
+                    .ToDictionaryAsync(u => u.ReportId, u => true);
+            }
         }
 
-        public async Task<IActionResult> OnPostDeleteAsync(int id)
+        public async Task<IActionResult> OnPostToggleUpvoteAsync(int reportId)
         {
-            var report = await _context.Reports.FindAsync(id);
-            if (report == null)
-            {
-                return NotFound();
-            }
-
-            var currentUser = await _userManager.GetUserAsync(User);
-            if (report.UserId != currentUser.Id)
+            var userId = _userManager.GetUserId(User);
+            if (userId == null)
             {
                 return Forbid();
             }
 
-            _context.Reports.Remove(report);
-            await _context.SaveChangesAsync();
+            var existingUpvote = await _context.Upvotes.FirstOrDefaultAsync(u => u.ReportId == reportId && u.UserId == userId);
+            var report = await _context.Reports.FindAsync(reportId);
+            if (existingUpvote != null)
+            {
+                _context.Upvotes.Remove(existingUpvote);
+                if (report != null)
+                {
+                    report.Upvotes--;
+                    _context.Reports.Update(report);
+                }
+            }
+            else
+            {
+                var upvote = new Upvote
+                {
+                    ReportId = reportId,
+                    UserId = userId
+                };
+                _context.Upvotes.Add(upvote);
+                if (report != null)
+                {
+                    report.Upvotes++;
+                    _context.Reports.Update(report);
+                }
+            }
 
+            await _context.SaveChangesAsync();
             return RedirectToPage();
         }
     }
