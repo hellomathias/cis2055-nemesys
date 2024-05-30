0dd62d549f0c412383d0104c8fcb8e7d6f2b043f
Mathias Schembri <mathiasschembri@gmail.com>
2024-05-30 11:31:31 +0200

    Add Upvote template

---
+++ NEMESYS/Areas/Identity/Pages/Reports/Models/Upvote.cs
@@ -0,0 +1,24 @@
+﻿using NEMESYS.Areas.Identity.Data;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NEMESYS.Areas.Identity.Pages.Reports.Models
+{
+    public class Upvote
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        [Required]
+        public int ReportId { get; set; }
+
+        [ForeignKey("UserId")]
+        public ApplicationUser User { get; set; }
+
+        [ForeignKey("ReportId")]
+        public Report Report { get; set; }
+    }
+}
