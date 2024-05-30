3156888ab52e6dedded90026e382c8c4bc5ea708
Mathias Schembri <mathiasschembri@gmail.com>
2024-05-30 09:51:33 +0200

    Move Hall of Fame Controller to Controllers Directory

---
+++ NEMESYS/Controllers/HallOfFameController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NEMESYS.Areas.Identity.Pages.HallOfFame.Models;
+using NEMESYS.Data;
+using System.Linq;
+using System;
+
+namespace NEMESYS.Areas.Identity.Controllers
+{
+    [Area("Identity")]
+    public class HallOfFameController : Controller
+    {
+        private readonly AuthDbContext _context;
+
+        public HallOfFameController(AuthDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var currentYear = DateTime.Now.Year;
+            var reports = _context.Reports
+                .Where(r => r.DateOfReport.Year == currentYear)
+                .ToList();
+
+            var reporterRankings = reports
+                .GroupBy(report => report.ReporterEmail)
+                .Select(group => new ReporterRanking
+                {
+                    ReporterName = group.Key,
+                    ReportCount = group.Count()
+                })
+                .OrderByDescending(r => r.ReportCount)
+                .ToList();
+
+            return View("~/Areas/Identity/Pages/HallOfFame/Index.cshtml", reporterRankings);
+        }
+    }
+}
