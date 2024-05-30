8860ce6fdfce71231c73e7f0bf5025da62625edd
Mathias Schembri <mathiasschembri@gmail.com>
2024-05-30 11:32:10 +0200

    Add UserController with Role Management for Administrators

---
+++ NEMESYS/Controllers/UserController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NEMESYS.Areas.Identity.Data;
+
+[Authorize(Roles = "Administrator")]
+public class UserController : Controller
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+
+    public UserController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+    {
+        _userManager = userManager;
+        _roleManager = roleManager;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var users = _userManager.Users.ToList();
+        var userRolesViewModel = new List<UserRolesViewModel>();
+
+        foreach (ApplicationUser user in users)
+        {
+            var thisViewModel = new UserRolesViewModel
+            {
+                UserId = user.Id,
+                Email = user.Email,
+                Roles = await GetUserRoles(user)
+            };
+            userRolesViewModel.Add(thisViewModel);
+        }
+        return View(userRolesViewModel);
+    }
+
+    private async Task<List<string>> GetUserRoles(ApplicationUser user)
+    {
+        return new List<string>(await _userManager.GetRolesAsync(user));
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(string roleName)
+    {
+        if (!string.IsNullOrEmpty(roleName))
+        {
+            var result = await _roleManager.CreateAsync(new IdentityRole(roleName));
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+        }
+        return View(roleName);
+    }
+}
+
+
+
